5d00a723ee07546d66baba75940af39bc1c8bb0f
fotso94 <s92fotso@gmail.com>
2025-08-12 17:03:15 -0400

    Update AppTest.java

--- src/test/java/DevOPS/devOPS/AppTest.java
+++ src/test/java/DevOPS/devOPS/AppTest.java
@@ -1,20 +1,12 @@
 package DevOPS.devOPS;
 
-import static org.junit.Assert.assertTrue;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-import org.junit.Test;
+public class AppTest {
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        Assert.assertTrue(true);
     }
 }

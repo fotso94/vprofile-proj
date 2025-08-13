e796ee59436716ad4e0f02dff4b182d617d27d44
fotso94 <s92fotso@gmail.com>
2025-08-12 23:49:06 -0400

    Update baseDriver.java

--- src/test/java/DevOPS/devOPS/baseDriver.java
+++ src/test/java/DevOPS/devOPS/baseDriver.java
@@ -1,27 +1,16 @@
-package DevOPS.devOPS;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class baseDriver {
-	
-	public baseDriver()
-	{
-		Chromedriver();
-	}
-	
-	
-	public static WebDriver Chromedriver() {
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")
-				  + "\\ChromeDriver\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		return driver;
+    public static WebDriver Chromedriver() {
+        // 1) REMOVE the System.setProperty line
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new", "--disable-gpu", "--window-size=1920,1080");
+        // optional on Windows builders:
+        options.addArguments("--no-sandbox", "--disable-dev-shm-usage");
 
-	}
-	
-	
-	
-
-	
+        // 2) This will trigger Selenium Manager to download the matching driver
+        return new ChromeDriver(options);
+    }
 }
